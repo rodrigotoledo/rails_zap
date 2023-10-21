bd48d9581bcc8e88be2a3358539e8985445cde58
Rodrigo Antonio de Souza Toledo <rodrigo@GLICNT62E0.local>
2023-10-21 06:51:35 -0300

    fxied layouts and application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   include CableReady::Broadcaster
   before_action :authenticate_user!
+
+  def set_layout
+  end
+
+  protected
+
+  def set_layout
+    devise_controller? ? 'devise' : 'application'
+  end
 end
