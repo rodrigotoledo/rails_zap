188737d8a7c7500260aadd8576d4ddab88cd0283
rtoledo_dev <rodrigo@rtoledo.inf.br>
2023-12-05 14:13:01 -0300

    setting the docker to application

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,9 +38,6 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :redis_cache_store, {
-      url: ENV.fetch("REDIS_URL") { "redis://redis:6379/1" }
-    }
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
@@ -49,6 +46,8 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+
+  config.cache_store = :redis_cache_store, { url: 'redis://redis:6379/0' } # Use o nome do serviço do contêiner Redis
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
