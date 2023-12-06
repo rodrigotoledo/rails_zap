bec348bd6214d27ac0cfb7a5fffd1ab6770872ec
rtoledo_dev <rodrigo@rtoledo.inf.br>
2023-12-05 22:01:28 -0300

    submit action and focus to form

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -1,7 +1,16 @@
 import ApplicationController from "controllers/application_controller"
 
 export default class extends ApplicationController {
+  // static targets = ["chat"]
   connect () {
     super.connect()
+    this.scrollMessagesToBottom()
+  }
+
+  scrollMessagesToBottom() {
+    const container = document.getElementById("chat")
+    const placeHolder = document.getElementById("message_message")
+    container.scrollTop = container.scrollHeight
+    placeHolder.focus()
   }
 }
